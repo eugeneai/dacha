51a5462ea6a1fe1644612cfe59095ce44ef781ec
Evgeny Cherkashin <eugeneai@irnok.net>
2018-01-28 09:41:26 +0800

    Added test file to test tests

---
+++ tests.py
@@ -0,0 +1,11 @@
+import os, sys, codecs
+if os.name=='nt':
+    sys.stdout = codecs.getwriter("utf-8")(sys.stdout.detach())
+
+class TestBasic:
+    def test_test(self):
+        print ("Test of console output")
+        print ("Тестирование вывода в консоль")
+        assert True
+
+
