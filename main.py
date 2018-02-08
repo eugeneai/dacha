85cbd8c542810ef4a27157fa11a3cf26103e713d
ISU Student <labs@irnok.net>
2018-02-08 06:19:40 +0000

    Formatting

--- main.py
+++ main.py
@@ -3,15 +3,16 @@ from PyQt5.QtWidgets import QApplication, QMainWindow
 from main_window import Ui_MainWindow
 from money_document import MoneyDocumentView
 from models import Document
-import os, codecs
-if os.name=='nt':
+import os
+import codecs
+if os.name == 'nt':
     sys.stdout = codecs.getwriter("utf-8")(sys.stdout.detach())
 
 
 class ApplicationWindow(QMainWindow):
     def __init__(self):
         super(ApplicationWindow, self).__init__()
-        self.ui=Ui_MainWindow()
+        self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
 
         self.ui.actionQuit.triggered.connect(self.quit_application)
@@ -21,16 +22,16 @@ class ApplicationWindow(QMainWindow):
         app.quit()
 
     def add_money_document(self, event):
-        doc=Document(1000)
+        doc = Document(1000)
         self.view_for_document(doc)
 
     def view_for_document(self, doc):
         doc_dial = MoneyDocumentView(doc)
-        doc.amount+=1000
+        doc.amount += 1000
         doc_dial.update()
-        rc=doc_dial.exec_()
+        rc = doc_dial.exec_()
         print("Now document amount is", doc.amount)
-        #if rc:
+        # if rc:
         #    print("Create Document and save it somwhere")
         #    print("Создать документ")
 
@@ -39,10 +40,10 @@ if __name__ == '__main__':
 
     app = QApplication(sys.argv)
 
-    w=ApplicationWindow()
+    w = ApplicationWindow()
     w.show()
 
     app.exec_()
-    #quit()
+    # quit()
     del w
     del app
